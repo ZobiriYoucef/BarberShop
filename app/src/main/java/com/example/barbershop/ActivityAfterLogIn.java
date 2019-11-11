1121c884456364fd6d582f4ce4d97610adfb1642
ZobiriYoucef <zy4live@outlook.com>
2019-11-11 14:58:41 +0100

    2.0

--- app/src/main/java/com/example/barbershop/ActivityAfterLogIn.java
+++ app/src/main/java/com/example/barbershop/ActivityAfterLogIn.java
@@ -1,34 +1,66 @@
 package com.example.barbershop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 import com.parse.ParseUser;
 
 public class ActivityAfterLogIn extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    private AppBarLayout appBarLayout;
+
+    private androidx.appcompat.widget.Toolbar myToolBar;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabAdopter tabAdopter;
 
     private Button btnLogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_log_in);
-        btnLogOut=findViewById(R.id.btnLogOut);
-        btnLogOut.setOnClickListener(this);
+        setTitle("Title");
+        findViews();
+        //btnLogOut=findViewById(R.id.btnLogOut);
+        //btnLogOut.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        /*switch (v.getId()){
             case(R.id.btnLogOut):
                 if (ParseUser.getCurrentUser()!=null){
                     ParseUser.getCurrentUser().logOut();
                     finish();
                 }
                 break;
-        }
+        }*/
     }
+
+
+    private void findViews() {
+        appBarLayout = (AppBarLayout)findViewById( R.id.appBarLayout );
+
+        myToolBar = findViewById( R.id.myToolBar );
+        setSupportActionBar(myToolBar);
+
+        viewPager=findViewById( R.id.viewPager );
+        tabAdopter =new TabAdopter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdopter);
+
+        tabLayout = findViewById( R.id.tabLayout );
+        tabLayout.setupWithViewPager(viewPager,false);
+    }
+
 }
--- app/src/main/java/com/example/barbershop/TabAdopter.java
+++ app/src/main/java/com/example/barbershop/TabAdopter.java
@@ -1,17 +1,15 @@
 package com.example.barbershop;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class TabAdopter extends FragmentPagerAdapter {
-    public TabAdopter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+    public TabAdopter( FragmentManager fm) {
+        super(fm);
     }
 
-    @NonNull
+
     @Override
     public Fragment getItem(int tabposition) {
         switch (tabposition){
@@ -20,28 +18,30 @@ public class TabAdopter extends FragmentPagerAdapter {
                 return profile_tab;
 
             case 1:
-                User_Tab user_tab = new User_Tab();
-                return user_tab;
+                return new User_Tab();
 
             case 2:
                 Share_Tab share_tab=new Share_Tab();
                 return share_tab;
+            default: return null;
 
         }
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 3;
     }
 
-    @Nullable
+
     @Override
     public CharSequence getPageTitle(int position) {
         switch (position){
             case 0: return "Profile";
             case 1: return "User";
             case 2: return "Shared";
+            default: return null;
 
         }
 
