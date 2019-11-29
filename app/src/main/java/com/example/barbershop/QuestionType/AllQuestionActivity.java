db1cf851e5232ed9d4a3877cfe8a8734371de654
ZobiriYoucef <zy4live@outlook.com>
2019-11-29 18:31:56 +0100

    Circl erro

--- app/src/main/java/com/example/barbershop/QuestionType/AllQuestionActivity.java
+++ app/src/main/java/com/example/barbershop/QuestionType/AllQuestionActivity.java
@@ -1,7 +1,9 @@
 package com.example.barbershop.QuestionType;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -11,10 +13,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.barbershop.Answers.Answers;
+import com.example.barbershop.MainActivity;
 import com.example.barbershop.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AllQuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,6 +29,8 @@ public class AllQuestionActivity extends AppCompatActivity implements View.OnCli
     FrameLayout fragmentContainerId1;
     @BindView(R.id.fragmentContainerId2)
     FrameLayout fragmentContainerId2;
+    @BindView(R.id.button2)
+    Button GoToCreatQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +44,13 @@ public class AllQuestionActivity extends AppCompatActivity implements View.OnCli
         answers3.selectedAnswerList.add("xxxxxx");
         answers3.selectedAnswerList.add("fffffffff");
 
-        addNewFragment(fragmentContainerId1,new Multiple_choice_CheckBox(answers3.selectedAnswerList));
+        addNewFragment(fragmentContainerId1, new Multiple_choice_CheckBox(answers3.selectedAnswerList));
         allQuestionsFragmentQuestionTextId.setOnClickListener(this);
+        GoToCreatQuestions.setOnClickListener(this);
 
     }
 
-    public void addNewFragment(FrameLayout frameLayout, Fragment fragment){
+    public void addNewFragment(FrameLayout frameLayout, Fragment fragment) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(frameLayout.getId(), fragment);
@@ -58,7 +65,13 @@ public class AllQuestionActivity extends AppCompatActivity implements View.OnCli
                 answers4.selectedAnswerList.add("595");
                 answers4.selectedAnswerList.add("sssssf");
                 answers4.selectedAnswerList.add("qqqqqqqqqqqqq");
-                addNewFragment(fragmentContainerId2,new Multiple_choice_CheckBox(answers4.selectedAnswerList));
+                addNewFragment(fragmentContainerId2, new Multiple_choice_CheckBox(answers4.selectedAnswerList));
+                break;
+
+            case R.id.button2:
+               Intent intent = new Intent(AllQuestionActivity.this, QuestionsCreature.class);
+               startActivity(intent);
+               break;
         }
     }
 }
--- app/src/main/java/com/example/barbershop/QuestionType/QuestionsCreature.java
+++ app/src/main/java/com/example/barbershop/QuestionType/QuestionsCreature.java
@@ -1,16 +1,64 @@
 package com.example.barbershop.QuestionType;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.barbershop.R;
+import com.github.clans.fab.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
-public class QuestionsCreature extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class QuestionsCreature extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_creature);
+
+
+    }
+    //Create a button to attach the menu:
+    ImageView icon = new ImageView(this); // Create an icon
+        icon.setImageResource(R.drawable.plus);
+    FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+            .setContentView(icon)
+            .build();
+    //Create menu items:
+    SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+    AddFloatingActionButton(itemBuilder,"AddQuestion",R.drawable.addquestion);
+    AddFloatingActionButton(itemBuilder,"AddQuestionChoise",R.drawable.addquestionchoise);
+    AddFloatingActionButton(itemBuilder,"AddQuestionRules",R.drawable.addquestionrules);
+    //Create the menu with the items:
+    FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+            .addSubActionView(subActionButton.get("AddQuestion"))
+            .addSubActionView(subActionButton.get("AddQuestionChoise"))
+            .addSubActionView(subActionButton.get("AddQuestionRules"))
+            .attachTo(actionButton)
+            .build();
+
+    private final Map<String, SubActionButton> subActionButton = new HashMap<>();
+    public void AddFloatingActionButton(SubActionButton.Builder itemBuilder,String buttonName,int ImageResource){
+        FrameLayout.LayoutParams params=new FrameLayout.LayoutParams(20, 20);
+        ImageView itemIcon = new ImageView(this);
+        itemIcon.setImageResource(ImageResource);
+        SubActionButton button = itemBuilder.setContentView(itemIcon)
+                .setTheme(3)
+                .build();
+        subActionButton.put(buttonName,button);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
+
+
